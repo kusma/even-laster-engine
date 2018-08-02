953eeb4c4938eb4c5e3b7c6e4f0e74bbb1969d8a
Erik Faye-Lund <kusmabite@gmail.com>
2018-08-02 22:58:45 +0200

    symlink assets-folder into build folder until assets-handling is finished

---
+++ bin/create_link.py
@@ -0,0 +1,15 @@
+#!/bin/env python3
+import os
+import argparse
+
+def main():
+    parser = argparse.ArgumentParser(description='Convert Rocket XML files into track-files.')
+    parser.add_argument('source', metavar='SOURCE')
+    parser.add_argument('link', metavar='LINK')
+    args = parser.parse_args()
+    if os.path.islink(args.link):
+        os.unlink(args.link)
+    os.symlink(args.source, args.link, True)
+
+if __name__ == "__main__":
+    main()
