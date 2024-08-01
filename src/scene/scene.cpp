1a0102e5c641d0340ebb55bd383a68851f0f6dfc
Erik Faye-Lund <kusmabite@gmail.com>
2024-08-01 15:08:19 +0200

    use staging buffer for index buffers also

--- src/scene/scene.cpp
+++ src/scene/scene.cpp
@@ -18,8 +18,11 @@ IndexedBatch meshToIndexedBatch(const Mesh &mesh)
 	auto vertexBuffer = new Buffer(vertices.size(), VK_BUFFER_USAGE_VERTEX_BUFFER_BIT | VK_BUFFER_USAGE_TRANSFER_DST_BIT, VK_MEMORY_PROPERTY_DEVICE_LOCAL_BIT);
 	vertexBuffer->uploadFromStagingBuffer(vertexStagingBuffer, 0, 0, vertices.size());
 
-	auto indexBuffer = new Buffer(indices.size(), VK_BUFFER_USAGE_INDEX_BUFFER_BIT, VK_MEMORY_PROPERTY_HOST_VISIBLE_BIT);
-	indexBuffer->uploadMemory(0, indices.data(), indices.size());
+	auto indexStagingBuffer = new StagingBuffer(indices.size());
+	indexStagingBuffer->uploadMemory(0, indices.data(), indices.size());
+
+	auto indexBuffer = new Buffer(indices.size(), VK_BUFFER_USAGE_INDEX_BUFFER_BIT | VK_BUFFER_USAGE_TRANSFER_DST_BIT, VK_MEMORY_PROPERTY_DEVICE_LOCAL_BIT);
+	indexBuffer->uploadFromStagingBuffer(indexStagingBuffer, 0, 0, indices.size());
 
 	VkIndexType indexType = VK_INDEX_TYPE_UINT16; // dummy
 	uint32_t indexCount = 0;
