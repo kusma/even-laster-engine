42f953c8c35570ff294fdde73c07e27c02ff8272
Erik Faye-Lund <kusmabite@gmail.com>
2024-08-06 13:34:20 +0200

    fix warning

--- src/main.cpp
+++ src/main.cpp
@@ -475,7 +475,7 @@ int main(int argc, char *argv[])
 		glfwGetFramebufferSize(win, &swapWidth, &swapHeight);
 		auto swapChain = SwapChain(surface, swapWidth, swapHeight, VK_IMAGE_USAGE_COLOR_ATTACHMENT_BIT | VK_IMAGE_USAGE_TRANSFER_DST_BIT);
 
-		const float demo_aspect = 16.0 / 9.0;
+		const float demo_aspect = 16.0f / 9;
 		float monitor_aspect = demo_aspect;
 		if (fullscreen) {
 			int width_mm, height_mm;
@@ -483,7 +483,7 @@ int main(int argc, char *argv[])
 			monitor_aspect = float(width_mm) / height_mm;
 		}
 
-		VkRect2D letterbox = makeLetterbox(swapWidth, swapHeight, monitor_aspect, 16.0 / 9);
+		VkRect2D letterbox = makeLetterbox(swapWidth, swapHeight, monitor_aspect, 16.0f / 9);
 
 		vector<VkFormat> depthCandidates = {
 			VK_FORMAT_D32_SFLOAT,
