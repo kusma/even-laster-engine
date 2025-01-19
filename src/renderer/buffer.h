8d9aa02009d3edc5f3aed645c2baba4d2996a0d5
Erik Faye-Lund <erik.faye-lund@collabora.com>
2025-01-19 11:09:50 +0100

    squash! upload ubos inline in cmdbuf
    Ugh, we're not doing this inline for the scene-renderer...

--- src/renderer/buffer.h
+++ src/renderer/buffer.h
@@ -61,7 +61,7 @@ public:
 
 class UniformBuffer : public Buffer {
 public:
-	UniformBuffer(VkDeviceSize size) : Buffer(size, VK_BUFFER_USAGE_UNIFORM_BUFFER_BIT | VK_BUFFER_USAGE_TRANSFER_DST_BIT, VMA_MEMORY_USAGE_AUTO, 0)
+	UniformBuffer(VkDeviceSize size) : Buffer(size, VK_BUFFER_USAGE_UNIFORM_BUFFER_BIT | VK_BUFFER_USAGE_TRANSFER_DST_BIT, VMA_MEMORY_USAGE_AUTO, VMA_ALLOCATION_CREATE_HOST_ACCESS_SEQUENTIAL_WRITE_BIT)
 	{
 	}
 };
