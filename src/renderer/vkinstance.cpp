3fbc0674f670ca7b7e6109e25e700ae51b9acd58
Erik Faye-Lund <erik.faye-lund@collabora.com>
2025-10-20 13:44:25 +0200

    yolo

--- src/renderer/vkinstance.cpp
+++ src/renderer/vkinstance.cpp
@@ -128,7 +128,6 @@ void vkInstance::deviceInit(VkPhysicalDevice physicalDevice, function<bool(VkIns
 
 	enabledFeatures.samplerAnisotropy = physicalDeviceFeatures.samplerAnisotropy;
 	enabledFeatures.geometryShader = true;
-	enabledFeatures.shaderStorageImageWriteWithoutFormat = true;
 
 	vkGetPhysicalDeviceProperties(physicalDevice, &deviceProperties);
 
